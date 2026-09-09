a9f74bbe0f97779da0115366bc5f0fa91d750c34
417-31-Nongnaphat <56785@yupparaj.ac.th>
2026-09-09 11:20:03 +0700

    vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -102,6 +102,6 @@ if st.session_state.get("is_ended", False):
     show_result_dialog(ans1, ans2)
 
 st.divider()
-st.write("นางสาวดีใจ ยิ้มแย้ม เลขที่ 5 ม.4/5")
+st.write("นางสาวนงนภัส วงศ์แสง เลขที่ 31 ม.4/17")
 
 
